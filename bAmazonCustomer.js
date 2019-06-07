6328bc2370d1749a74038625b564fb5eaddefecc
ZacWarner <zektgn@gmail.com>
2019-06-06 19:56:34 -0700

    completed customer app for bAmazon

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -33,7 +33,6 @@ connection.connect(function (err) {
 
 function start() {
     inquirer.prompt([
-
         {
             type: "list",
             message: "what would you like to do?",
@@ -63,9 +62,48 @@ function itemList() {
             )
         }
         console.log(table.toString());
+        start();
     })
 };
 
 function buy() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What's the number of the item you would like to buy?",
+            name: "itemNum"
+        },
+        {
+            type: "input",
+            message: "How many would you like to buy?",
+            name: "quantity"
+        }
+    ]).then(function (inqRes) {
+        var purch = inqRes.quantity;
+        connection.query("SELECT stock_quantity FROM products WHERE item_id = \"" + inqRes.itemNum + "\"", function (err, res) {
+            var stock = res[0].stock_quantity;
+            if (purch <= stock) {
+                console.log("you Bought " + purch + "items!");
+                var left = stock - purch;
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    {
+                        stock_quantity: left,
+                    },
+                    {
+                        item_id: inqRes.itemNum,
+                    }
+                ]);
+                start();
+            }
+            else if (purch > stock) {
+                var over = purch - stock;
+                console.log("Sorry theres not enough stock, reduce order by " + over);
+                start();
+            }
+
+
+
+        });
+    });
 
 };
